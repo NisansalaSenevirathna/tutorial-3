3a144280ac02c8f321fd85f3c9d5618f995d81d3
NisansalaSenevirathna <92750030+NisansalaSenevirathna@users.noreply.github.com>
2022-07-20 03:36:23 +0530

    Update Q4.scala

--- Q4.scala
+++ Q4.scala
@@ -2,7 +2,7 @@ object Q4 extends App{
 
   def book_price(copies:Int):Double=copies * 24.95; //total amount for x books
   def discount(amount:Double):Double=amount * 40/100;
-  def shopping_cost(copies:Int):Double= if(x<=50)3 + else 3+(copies-50) * 0.75;
+  def shopping_cost(copies:Int):Double= if(x<=50)3  else 3*50(copies-50) * 0.75;
   def total_cost(copies :Int):Double =book_price(copies) - discount(book_price(copies)) + shopping_cost(copies);
 
   printf("\nTotal cost is:%.2f\n",total_cost(60));
